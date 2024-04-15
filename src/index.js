748900ab643037a0566f603cb8987bd9a9292ca5
Your Name <you@example.com>
2024-04-15 20:44:01 +0530

    Fix line endings in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-export const server = "http://localhost:3000/api/v1";
+export const server = "https://rfq1-1.onrender.com/api/v1";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
